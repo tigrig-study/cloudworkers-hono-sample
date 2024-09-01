6a25821f19cf12eb154a9744fd506887280b0a70
TigRig <tigrig.crex@gmail.com>
2024-09-01 22:54:53 +0900

    Hello World

--- cloudworkers-hono-sample/src/index.ts
+++ cloudworkers-hono-sample/src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from 'hono'
 const app = new Hono<{ Bindings: CloudflareBindings }>()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello World!')
 })
 
 export default app
